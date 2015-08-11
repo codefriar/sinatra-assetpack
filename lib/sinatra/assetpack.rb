9cf46fd9146d80e0aff3c4ee7f347fda119a63df
Kevin Poorman <kpoorman@salesforce.com>
2015-08-10 22:44:07 -0500

    updating to new tilt api

--- lib/sinatra/assetpack.rb
+++ lib/sinatra/assetpack.rb
@@ -17,10 +17,10 @@ module Sinatra
     def self.tilt_formats
       @formats ||= begin
         hash = Hash.new
-        Tilt.mappings.each do |format, (engine, _)|
+        Tilt.default_mapping.each do |format, (engine, _)|
           # @todo Remove when fix is merged in tilt
           # https://github.com/rtomayko/tilt/pull/206
-          next if engine.nil? 
+          next if engine.nil?
           case engine.default_mime_type
           when 'text/css' then hash[format] = 'css'
           when 'application/javascript' then hash[format] = 'js'
